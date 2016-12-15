fbb811008c5d42c4c15771a44868486357b1087c
Zhang Qiang <zhangqiangoffice@163.com>
2016-12-15 12:07:10 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const objToParams = (obj) => {
+  let list = [];
+  for (var key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      list.push(`${key}=${obj[key]}`);
+    }
+  }
+  return list.join('&');
+}
+
+const zAJAX = (url, datas, func) => {
+  let xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("POST", url, true);
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xmlhttp.send(objToParams(datas));
+  xmlhttp.onreadystatechange = () => {
+    if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+      func(JSON.parse(xmlhttp.responseText));
+    }
+  }
+}
+
+export default zAJAX
